eeaa7c7267ed746e0cb091927805561791d582c0
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-05-20 07:59:13 +0200

    Calculating the local sum of squared differences

--- standard_deviation.c
+++ standard_deviation.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "mpi.h"
 
 int main(int argc , char * argv[])
@@ -10,7 +11,7 @@ int main(int argc , char * argv[])
     int i;
     int local_sum = 0, global_sum = 0;
     int* elements;
-    float global_mean = 0;
+    float global_mean = 0, local_sum_sqrd_diff = 0;
     MPI_Status status;	/* return status for receive */
 
     /* Start up MPI */
@@ -40,28 +41,36 @@ int main(int argc , char * argv[])
         elements = malloc(n * sizeof (int));
         local_sum = 0;
 
-        // different seed for each process
+        // Different seed for each process
         srand(time(NULL) + my_rank);
         printf("P%d elements are: ", my_rank);
 
+        // Generating random elements for each process
         for(i = 0 ; i < n ; i++){
             elements[i] = rand() % 100;
             printf("%d\t", elements[i]);
             local_sum += elements[i];
         }
-
-        printf("\nlocal_sum = %d\n\n", local_sum);
-
+        printf("\n");
     }
 
+    /* Calculating the sum of all local sums to the global sum */
     MPI_Allreduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
-    global_mean = ((float)global_sum) / (n * (p - 1));
 
-    printf("Global mean @P%d = %f\n", my_rank, global_mean);
-
-    if(my_rank == 0)
+    if(my_rank != 0)
     {
+        local_sum_sqrd_diff = 0;
+
+        // Calculating the global mean
+        global_mean = ((float)global_sum) / (n * (p - 1));
+
+        // Calculating the local sum of squared differences
+        for(i = 0 ; i < n ; i++)
+            local_sum_sqrd_diff += pow(elements[i] - global_mean, 2);
+
+
+        printf("sum_sqrd_diff @P%d = %f\n", my_rank, local_sum_sqrd_diff);
 
     }
 

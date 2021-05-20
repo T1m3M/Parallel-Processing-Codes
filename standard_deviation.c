36e2d64e6e2bd4f4290df76a29ab233669ac78bd
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-05-20 05:58:12 +0200

    Generating random numbers and calculating summation

--- standard_deviation.c
+++ standard_deviation.c
@@ -7,6 +7,9 @@ int main(int argc , char * argv[])
     int my_rank;		/* rank of process */
     int p;			    /* number of process */
     int n;
+    int i;
+    int local_sum;
+    int* elements;
     MPI_Status status;	/* return status for receive */
 
     /* Start up MPI */
@@ -27,14 +30,28 @@ int main(int argc , char * argv[])
     /* Broadcasting n to each slave process */
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if( my_rank == 0)
+    if(my_rank == 0)
     {
 
     }
     else
     {
-        printf("From P%d n is: %d\n", my_rank, n);
+        elements = malloc(n * sizeof (int));
+        local_sum = 0;
+
+        // different seed for each process
+        srand(time(NULL) + my_rank);
+        printf("P%d elements are: ", my_rank);
+
+        for(i = 0 ; i < n ; i++){
+            elements[i] = rand() % 100;
+            printf("%d\t", elements[i]);
+            local_sum += elements[i];
+        }
+        printf("\n");
     }
+
+    //MPI_Allreduce(&local_sum, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     /* shutdown MPI */
     MPI_Finalize();

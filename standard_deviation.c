a686a4d23b3c50b2fb9bba86772bab008ea7fedd
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-05-20 06:40:58 +0200

    Calculating global mean

--- standard_deviation.c
+++ standard_deviation.c
@@ -8,8 +8,9 @@ int main(int argc , char * argv[])
     int p;			    /* number of process */
     int n;
     int i;
-    int local_sum;
+    int local_sum = 0, global_sum = 0;
     int* elements;
+    float global_mean = 0;
     MPI_Status status;	/* return status for receive */
 
     /* Start up MPI */
@@ -48,12 +49,24 @@ int main(int argc , char * argv[])
             printf("%d\t", elements[i]);
             local_sum += elements[i];
         }
-        printf("\n");
+
+        printf("\nlocal_sum = %d\n\n", local_sum);
+
     }
 
-    //MPI_Allreduce(&local_sum, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Allreduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    global_mean = ((float)global_sum) / (n * (p - 1));
+
+    printf("Global mean @P%d = %f\n", my_rank, global_mean);
+
+    if(my_rank == 0)
+    {
+
+    }
 
     /* shutdown MPI */
     MPI_Finalize();
+
     return 0;
 }
